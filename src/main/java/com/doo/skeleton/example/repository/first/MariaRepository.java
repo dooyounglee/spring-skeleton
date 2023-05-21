e151622920d774e4217e709c37184b4750ec4b14
doo <gostbaducking1@nate.com>
2023-05-22 00:48:59 +0900

    fix: delete not used line

--- src/main/java/com/doo/skeleton/example/repository/first/MariaRepository.java
+++ src/main/java/com/doo/skeleton/example/repository/first/MariaRepository.java
@@ -1,7 +1,6 @@
 package com.doo.skeleton.example.repository.first;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.doo.skeleton.example.dto.db.first.MariaDto;
 
--- src/main/java/com/doo/skeleton/example/repository/second/MysqlRepository.java
+++ src/main/java/com/doo/skeleton/example/repository/second/MysqlRepository.java
@@ -1,7 +1,6 @@
 package com.doo.skeleton.example.repository.second;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import com.doo.skeleton.example.dto.db.second.MysqlDto;
 
