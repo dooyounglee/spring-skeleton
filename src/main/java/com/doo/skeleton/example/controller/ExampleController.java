a182dd7c82ad270d59ddf79e4d5ec05043ed6615
doo <gostbaducking1@nate.com>
2023-05-22 00:50:41 +0900

    feat: controller,service method추가

--- src/main/java/com/doo/skeleton/example/controller/ExampleController.java
+++ src/main/java/com/doo/skeleton/example/controller/ExampleController.java
@@ -2,6 +2,7 @@ package com.doo.skeleton.example.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -11,16 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.doo.skeleton.example.dto.RequestDto;
 import com.doo.skeleton.example.dto.ResponseDto;
+import com.doo.skeleton.example.service.ExampleService;
 
 @RestController
 @RequestMapping("/api/v1/example")
 public class ExampleController {
 	
-	private final Logger logger = LoggerFactory.getLogger(ExampleController.class);
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	
+	@Autowired
+	private ExampleService exampleService;
 
 	@RequestMapping(value = "/example1", method = RequestMethod.GET)
-	public ResponseEntity<ResponseDto> example1(RequestDto requestDto) {
-		logger.debug("com.doo.skeleton.example.controller.ExampleController.example1.requestDto : {}", requestDto);
+	public ResponseEntity<ResponseDto> getExample1(RequestDto requestDto) {
+		logger.debug("com.doo.skeleton.example.controller.ExampleController.getExample1.requestDto : {}", requestDto);
+		
+		exampleService.getVoid();
+		exampleService.get();
 		
 		ResponseDto responseDto = new ResponseDto();
 		
@@ -29,9 +37,12 @@ public class ExampleController {
 				.body(responseDto);
 	}
 	
-	@RequestMapping(value = "/example2", method = RequestMethod.POST)
-	public ResponseEntity<ResponseDto> example2(@RequestBody RequestDto requestDto) {
-		logger.debug("com.doo.skeleton.example.controller.ExampleController.example2.requestDto : {}", requestDto);
+	@RequestMapping(value = "/example1", method = RequestMethod.POST)
+	public ResponseEntity<ResponseDto> postExample1(@RequestBody RequestDto requestDto) {
+		logger.debug("com.doo.skeleton.example.controller.ExampleController.postExample1.requestDto : {}", requestDto);
+		
+		exampleService.postVoid();
+		exampleService.post();
 		
 		ResponseDto responseDto = new ResponseDto();
 		
@@ -40,9 +51,26 @@ public class ExampleController {
 				.body(responseDto);
 	}
 	
-	@RequestMapping(value = "/example3", method = RequestMethod.PUT)
-	public ResponseEntity<ResponseDto> example3(@RequestBody RequestDto requestDto) {
-		logger.debug("com.doo.skeleton.example.controller.ExampleController.example3.requestDto : {}", requestDto);
+	@RequestMapping(value = "/example1", method = RequestMethod.PUT)
+	public ResponseEntity<ResponseDto> putExample1(@RequestBody RequestDto requestDto) {
+		logger.debug("com.doo.skeleton.example.controller.ExampleController.putExample1.requestDto : {}", requestDto);
+		
+		exampleService.putVoid();
+		exampleService.put();
+		
+		ResponseDto responseDto = new ResponseDto();
+		
+		return ResponseEntity
+				.status(HttpStatus.ACCEPTED)
+				.body(responseDto);
+	}
+	
+	@RequestMapping(value = "/example1", method = RequestMethod.DELETE)
+	public ResponseEntity<ResponseDto> deleteExample1(@RequestBody RequestDto requestDto) {
+		logger.debug("com.doo.skeleton.example.controller.ExampleController.deleteExample1.requestDto : {}", requestDto);
+		
+		exampleService.deleteVoid();
+		exampleService.delete();
 		
 		ResponseDto responseDto = new ResponseDto();
 		
---
+++ src/main/java/com/doo/skeleton/example/service/ExampleService.java
@@ -0,0 +1,41 @@
+package com.doo.skeleton.example.service;
+
+import org.springframework.stereotype.Service;
+
+import com.doo.skeleton.example.dto.ResponseDto;
+
+@Service
+public class ExampleService {
+
+	public void getVoid() {
+		
+	}
+	
+	public ResponseDto get() {
+		return new ResponseDto();
+	}
+	
+	public void postVoid() {
+		
+	}
+	
+	public ResponseDto post() {
+		return new ResponseDto();
+	}
+	
+	public void putVoid() {
+		
+	}
+	
+	public ResponseDto put() {
+		return new ResponseDto();
+	}
+	
+	public void deleteVoid() {
+		
+	}
+	
+	public ResponseDto delete() {
+		return new ResponseDto();
+	}
+}
