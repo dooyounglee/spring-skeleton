1588f4bcf2dac2097945d94974bc741ae301b13c
doo <gostbaducking1@nate.com>
2023-05-23 01:05:18 +0900

    feat: jpa(maria,mysql,mongodb) repository test 추가

---
+++ src/test/java/com/doo/skeleton/example/repository/first/MariaRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.doo.skeleton.example.repository.first;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.doo.skeleton.db.config.FirstDataConfig;
+import com.doo.skeleton.example.dto.db.first.MariaDto;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@ImportAutoConfiguration(classes = FirstDataConfig.class)
+public class MariaRepositoryTest {
+
+	@Autowired
+	private MariaRepository mariaRepository;
+	
+	@Test
+	@Transactional("firstTransactionManager")
+	public void saveTest() {
+		MariaDto result = mariaRepository.save(new MariaDto());
+		assertEquals(result.getId(), 1);
+	}
+}
---
+++ src/test/java/com/doo/skeleton/example/repository/second/MysqlRepositoryTest.java
@@ -0,0 +1,30 @@
+package com.doo.skeleton.example.repository.second;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.doo.skeleton.db.config.SecondDataConfig;
+import com.doo.skeleton.example.dto.db.second.MysqlDto;
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@ImportAutoConfiguration(classes = SecondDataConfig.class)
+public class MysqlRepositoryTest {
+
+	@Autowired
+	private MysqlRepository mysqlRepository;
+	
+	@Test
+	@Transactional("secondTransactionManager")
+	public void saveTest() {
+		MysqlDto result = mysqlRepository.save(new MysqlDto());
+		assertEquals(result.getId(), 1);
+	}
+}
