8a80c11f79684c85857f73dba84edf4b7aaa69c5
doo <gostbaducking1@nate.com>
2023-05-23 01:04:37 +0900

    fix: document명 수정, 생성자 추가

--- src/main/java/com/doo/skeleton/example/dto/db/fourth/MongoDto.java
+++ src/main/java/com/doo/skeleton/example/dto/db/fourth/MongoDto.java
@@ -3,10 +3,14 @@ package com.doo.skeleton.example.dto.db.fourth;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
-@Document(collection = "mongdoDto")
+@Document(collection = "mongoDto")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class MongoDto {
 
 	@Id private String id;
