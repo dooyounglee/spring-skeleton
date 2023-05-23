35dd2012fdca2b3cd17044bac12526da3ed56eca
doo <gostbaducking1@nate.com>
2023-05-24 00:48:39 +0900

    fix: 생성자 추가

--- src/main/java/com/doo/skeleton/example/dto/db/sixth/OracleDto.java
+++ src/main/java/com/doo/skeleton/example/dto/db/sixth/OracleDto.java
@@ -1,8 +1,12 @@
 package com.doo.skeleton.example.dto.db.sixth;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class OracleDto {
 
 	private int id;
--- src/main/java/com/doo/skeleton/example/dto/db/third/Db2Dto.java
+++ src/main/java/com/doo/skeleton/example/dto/db/third/Db2Dto.java
@@ -1,8 +1,12 @@
 package com.doo.skeleton.example.dto.db.third;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Db2Dto {
 
 	private String id;
