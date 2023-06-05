e9553e594c01126c2d0979c7fec78c67be93a145
doo <gostbaducking1@nate.com>
2023-06-06 01:07:55 +0900

    feat: BusinessException 추가

---
+++ src/main/java/com/doo/skeleton/exception/BusinessExceptionHandler.java
@@ -0,0 +1,48 @@
+package com.doo.skeleton.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.doo.skeleton.util.MessageUtil;
+
+@RestControllerAdvice
+public class BusinessExceptionHandler {
+
+	@ExceptionHandler(value = { BusinessException.class })
+	public ResponseEntity<Object> handlerBusinessException(BusinessException e) {
+		e.printStackTrace();
+		
+		e.setErrorCode(-100);
+		
+		return ResponseEntity
+				.status(HttpStatus.EXPECTATION_FAILED)
+				.body(e);
+	}
+	
+	@ExceptionHandler(value = { RuntimeException.class })
+	public ResponseEntity<BusinessException> handlerRuntimeException(RuntimeException e) {
+		e.printStackTrace();
+		
+		BusinessException be = new BusinessException("CM-0001");
+		be.setErrorCode(-1);
+		
+		return ResponseEntity
+				.status(HttpStatus.INTERNAL_SERVER_ERROR)
+				.body(be);
+	}
+	
+	@ExceptionHandler(value = { Exception.class })
+	public ResponseEntity<BusinessException> handlerException(Exception e) {
+		e.printStackTrace();
+		
+		BusinessException be = new BusinessException("CM-0001");
+		be.setErrorCode(-1);
+		be.setMessage(MessageUtil.getMessage("CM-0001", null, null));
+		
+		return ResponseEntity
+				.status(HttpStatus.INTERNAL_SERVER_ERROR)
+				.body(be);
+	}
+}
---
+++ src/test/java/com/doo/skeleton/exception/BusinessExceptionTest.java
@@ -0,0 +1,37 @@
+package com.doo.skeleton.exception;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BusinessExceptionTest {
+
+	@Test
+	public void exceptionTest() {
+		assertThrows(NullPointerException.class, () -> {
+			Map<String, Object> map = new HashMap<>();
+			map.get("dd").toString();
+		}, "aaaa");
+	}
+	
+	@Test
+	public void exception_messageTest() {
+		assertThrows(BusinessException.class, () -> {
+			throw new BusinessException("greeting", new Object[] {"java"}, null);
+		});
+	}
+	
+	@Test
+	public void classnameTest() {
+		System.out.println(this.getClass().descriptorString());
+		System.out.println(this.getClass().getName());
+		System.out.println(this.getClass().getPackageName());
+		System.out.println(this.getClass().getSimpleName());
+		System.out.println(this.getClass().getTypeName());
+	}
+}
