4b0697554a694aa71ebb15bfe92bf1b8efe0db8f
doo <gostbaducking1@nate.com>
2023-06-06 16:32:13 +0900

    fix: messageUtil test 추가

--- src/test/java/com/doo/skeleton/util/MessageUtilTest.java
+++ src/test/java/com/doo/skeleton/util/MessageUtilTest.java
@@ -13,17 +13,26 @@ public class MessageUtilTest {
 	@Test
 	public void getmessage() throws Exception {
 		
-		assertEquals(MessageUtil.getMessage("onlydefault", new String[]{"java1"}, null), 	"onlyDefault java1");
-		assertEquals(MessageUtil.getMessage("all", new String[]{"java1"}, null), 			"ko java1");
-		assertEquals(MessageUtil.getMessage("all", new String[]{"java1"}, Locale.KOREA), 	"ko java1");
-		assertEquals(MessageUtil.getMessage("all", new String[]{"java1"}, Locale.US), 		"en java1");
-		assertEquals(MessageUtil.getMessage("all", new String[]{"java1"}, Locale.ENGLISH), 	"en java1");
+		assertEquals("only java1", MessageUtil.getMessage("only", new String[]{"java1"}, null));
+		assertEquals("all java1",  MessageUtil.getMessage("all", new String[]{"java1"}, Locale.JAPANESE));
+		assertEquals("ko java1",   MessageUtil.getMessage("all", new String[]{"java1"}, null));
+		assertEquals("ko java1",   MessageUtil.getMessage("all", new String[]{"java1"}, Locale.KOREA));
+		assertEquals("en java1",   MessageUtil.getMessage("all", new String[]{"java1"}, Locale.US));
+		assertEquals("en java1",   MessageUtil.getMessage("all", new String[]{"java1"}, Locale.ENGLISH));
+		
+		assertEquals("ko1 java1", MessageUtil.getMessage("ko1", new String[]{"java1"}, null));
+		assertEquals("ko1 java1", MessageUtil.getMessage("ko1", new String[]{"java1"}, Locale.CANADA));
+		assertEquals("ko1 java1", MessageUtil.getMessage("ko1", new String[]{"java1"}, Locale.KOREAN));
+		assertEquals("ko1 java1", MessageUtil.getMessage("ko1", new String[]{"java1"}, Locale.ENGLISH));
+		
+		assertEquals("no", MessageUtil.getMessage("no", new String[]{"java1"}, null));
 	}
 	
 	@Test
 	public void getmessage1() throws Exception {
 		
-		assertEquals(MessageUtil.getMessageDB("CM-0001", null, null), "완료했씁니다.");
-		assertEquals(MessageUtil.getMessageDB("CM-0002", null, null), "하실래요?");
+		assertEquals("CM-0000",   MessageUtil.getMessageDB("CM-0000", null, null));
+		assertEquals("완료했씁니다.", MessageUtil.getMessageDB("CM-0001", null, null));
+		assertEquals("하실래요?",    MessageUtil.getMessageDB("CM-0002", null, null));
 	}
 }
