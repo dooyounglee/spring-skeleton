79c76fb2fcb6de4be27e2c6dd89cb9c644d9a5ab
doo <gostbaducking1@nate.com>
2023-06-14 01:10:59 +0900

    fix: @ResponseStatus 적용

--- src/main/java/com/doo/skeleton/example/controller/ExampleController.java
+++ src/main/java/com/doo/skeleton/example/controller/ExampleController.java
@@ -2,43 +2,43 @@ package com.doo.skeleton.example.controller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.doo.skeleton.example.dto.RequestDto;
 import com.doo.skeleton.example.dto.ResponseDto;
 import com.doo.skeleton.example.service.ExampleService;
 
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequestMapping("/api/v1/example")
+@RequiredArgsConstructor
 public class ExampleController {
 	
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 	
-	@Autowired
-	private ExampleService exampleService;
+	private final ExampleService exampleService;
 
+	@ResponseStatus(HttpStatus.ACCEPTED)
 	@RequestMapping(value = "/example1", method = RequestMethod.GET)
-	public ResponseEntity<ResponseDto> getExample1(RequestDto requestDto) {
+	public ResponseDto getExample1(RequestDto requestDto) {
 		logger.debug("com.doo.skeleton.example.controller.ExampleController.getExample1.requestDto : {}", requestDto);
+		ResponseDto responseDto = new ResponseDto();
 		
 		exampleService.getVoid();
 		exampleService.get();
 		
-		ResponseDto responseDto = new ResponseDto();
-		
-		return ResponseEntity
-				.status(HttpStatus.ACCEPTED)
-				.body(responseDto);
+		return responseDto;
 	}
 	
+	@ResponseStatus(HttpStatus.ACCEPTED)
 	@RequestMapping(value = "/example1", method = RequestMethod.POST)
-	public ResponseEntity<ResponseDto> postExample1(@RequestBody RequestDto requestDto) {
+	public ResponseDto postExample1(@RequestBody RequestDto requestDto) {
 		logger.debug("com.doo.skeleton.example.controller.ExampleController.postExample1.requestDto : {}", requestDto);
 		
 		exampleService.postVoid();
@@ -46,13 +46,12 @@ public class ExampleController {
 		
 		ResponseDto responseDto = new ResponseDto();
 		
-		return ResponseEntity
-				.status(HttpStatus.ACCEPTED)
-				.body(responseDto);
+		return responseDto;
 	}
 	
+	@ResponseStatus(HttpStatus.ACCEPTED)
 	@RequestMapping(value = "/example1", method = RequestMethod.PUT)
-	public ResponseEntity<ResponseDto> putExample1(@RequestBody RequestDto requestDto) {
+	public ResponseDto putExample1(@RequestBody RequestDto requestDto) {
 		logger.debug("com.doo.skeleton.example.controller.ExampleController.putExample1.requestDto : {}", requestDto);
 		
 		exampleService.putVoid();
@@ -60,13 +59,12 @@ public class ExampleController {
 		
 		ResponseDto responseDto = new ResponseDto();
 		
-		return ResponseEntity
-				.status(HttpStatus.ACCEPTED)
-				.body(responseDto);
+		return responseDto;
 	}
 	
+	@ResponseStatus(HttpStatus.ACCEPTED)
 	@RequestMapping(value = "/example1", method = RequestMethod.DELETE)
-	public ResponseEntity<ResponseDto> deleteExample1(@RequestBody RequestDto requestDto) {
+	public ResponseDto deleteExample1(@RequestBody RequestDto requestDto) {
 		logger.debug("com.doo.skeleton.example.controller.ExampleController.deleteExample1.requestDto : {}", requestDto);
 		
 		exampleService.deleteVoid();
@@ -74,8 +72,6 @@ public class ExampleController {
 		
 		ResponseDto responseDto = new ResponseDto();
 		
-		return ResponseEntity
-				.status(HttpStatus.ACCEPTED)
-				.body(responseDto);
+		return responseDto;
 	}
 }
