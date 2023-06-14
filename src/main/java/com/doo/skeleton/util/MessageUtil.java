2e4870a8e33a868acdf0d26b38192b9363f4a704
doo <gostbaducking1@nate.com>
2023-06-14 22:45:35 +0900

    fix: getMessage 파라미터 overloading

--- src/main/java/com/doo/skeleton/util/MessageUtil.java
+++ src/main/java/com/doo/skeleton/util/MessageUtil.java
@@ -9,6 +9,26 @@ public class MessageUtil {
 
 	private static Locale defulatLocale = Locale.KOREAN;
 	
+	public static String getMessage(String messageCode) {
+		MessageSource messageSource = (MessageSource) BeanUtil.getBean("messageSource");
+		
+		try {
+			return messageSource.getMessage(messageCode, null, defulatLocale);
+		} catch (NoSuchMessageException e) {
+			return messageCode;
+		}
+	}
+	
+	public static String getMessage(String messageCode, Object[] args) {
+		MessageSource messageSource = (MessageSource) BeanUtil.getBean("messageSource");
+		
+		try {
+			return messageSource.getMessage(messageCode, args, defulatLocale);
+		} catch (NoSuchMessageException e) {
+			return messageCode;
+		}
+	}
+	
 	public static String getMessage(String messageCode, Object[] args, Locale locale) {
 		if (locale == null) locale = defulatLocale;
 		MessageSource messageSource = (MessageSource) BeanUtil.getBean("messageSource");
@@ -18,6 +38,16 @@ public class MessageUtil {
 		} catch (NoSuchMessageException e) {
 			return messageCode;
 		}
+	}
+	
+	public static String getMessageDB(String messageCode) {
+		MessageSource messageSource = (MessageSource) BeanUtil.getBean("messageSourceDB");
+		return messageSource.getMessage(messageCode, null, defulatLocale);
+	}
+	
+	public static String getMessageDB(String messageCode, Object[] args) {
+		MessageSource messageSource = (MessageSource) BeanUtil.getBean("messageSourceDB");
+		return messageSource.getMessage(messageCode, args, defulatLocale);
 	}
 	
 	public static String getMessageDB(String messageCode, Object[] args, Locale locale) {
